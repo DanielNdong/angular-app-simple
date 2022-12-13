35f4742360adb0e40394a9795d429f523914d0ba
Daniel Aquilino Ndong <prowebmaking@outlook.es>
2022-12-13 15:41:42 +0100

    Add files via upload

--- app-project/src/app/app-routing.module.ts
+++ app-project/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from './pages/admin/admin.component';
-import { CurriculumComponent } from './pages/curriculum/curriculum.component';
+import { CurriculumVitaeComponent } from './pages/curriculum-vitae/curriculum-vitae.component';
 import { Error404Component } from './pages/error404/error404.component';
+import { FormularioComponent } from './pages/formulario/formulario.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { LogoutComponent } from './pages/logout/logout.component';
@@ -10,11 +10,11 @@ import { UserComponent } from './pages/user/user.component';
 
 const routes: Routes = [
   {path: '',component: HomeComponent},
-  {path: 'curriculum-vitae',component: CurriculumComponent},
+  {path: 'curriculum-vitae',component: CurriculumVitaeComponent},
   {path: 'login',component: LoginComponent},
+  {path: 'formulario',component: FormularioComponent},
   {path: 'logout',component: LogoutComponent},
   {path: 'user',component: UserComponent},
-  {path: 'admin',component: AdminComponent},
   {path: 'error404',component: Error404Component},
   {path: '**', pathMatch: 'full', redirectTo: 'error404'},//En caso de escribir mal una direccion
 ];
--- app-project/src/app/app.module.ts
+++ app-project/src/app/app.module.ts
@@ -5,30 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CurriculumComponent } from './pages/curriculum/curriculum.component';
 import { HomeComponent } from './pages/home/home.component';
 import { Error404Component } from './pages/error404/error404.component';
 import { HeaderUserComponent } from './components/headeruser/headeruser.component';
-import { HeaderAdminComponent } from './components/headeradmin/headeradmin.component';
 import { LoginComponent } from './pages/login/login.component';
 import { LogoutComponent } from './pages/logout/logout.component';
 import { UserComponent } from './pages/user/user.component';
-import { AdminComponent } from './pages/admin/admin.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { FormularioComponent } from './pages/formulario/formulario.component';
+import { CurriculumVitaeComponent } from './pages/curriculum-vitae/curriculum-vitae.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CurriculumComponent,
     HomeComponent,
     Error404Component,
     HeaderUserComponent,
-    HeaderAdminComponent,
     LoginComponent,
     LogoutComponent,
     UserComponent,
-    AdminComponent,
+    CardsComponent,
+    FormularioComponent,
+    CurriculumVitaeComponent,
+    ContactoComponent,
   ],
   imports: [
     BrowserModule,
